b434e870d188f79a3c6aa26abffb960cceab1c3f
Emre Demir <emre2548@gmail.com>
2021-05-29 17:35:55 +0300

    Business; added AutofacBusinessModule API request router Core; Added Interceptors for API request router

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+//using Business.CCS;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+//using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        /* WebAPI içerisinde 
+         * services.AddSingleton<IProductService, ProductManager>();
+         *   services.AddSingleton<IProductDal, EfProductDal>(); 
+         * kodlarını yazdığım yeri burada yapacağız */
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+        }
+
+    }
+}
